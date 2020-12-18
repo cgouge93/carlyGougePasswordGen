f262a5377a590e866a0b14cbac6324e817938674
Carly G <73192129+cgouge93@users.noreply.github.com>
2020-12-17 19:35:47 -0600

    added for loop to generate pw of desired length

--- script.js
+++ script.js
@@ -29,21 +29,48 @@ function getRandomSpecial() {
 }
 
 
-
+// Begin generate password function
 function generatePassword() {
+
+let newPassword = '';
+let passwordSet = '';
 // Prompting user for input
 charNum = parseInt(prompt("How many characters would you like your password to be? Minimum 8, maximum 128."));
 
-// If statement for validation
+// If statements for validation
 if (!charNum){
   alert("You must enter a value!")
 } else if (charNum < 9 || charNum > 129) {
   charNum = parseInt(prompt("Minimum of 8 characters, maximum of 128 characters."));
-
-} else {
-  confirmNum = confirm("Do you want your password to contain numbers?");
-  confirmChar = confirm("Do you want your password to contain special characters?");
-  confirmLower = confirm("Do you want your password to contain lowercase letters?");
+  return;
+} 
+confirmNum = confirm("Do you want your password to contain numbers?");
+if (confirmNum) {
+  passwordSet += numerical;
+}
+confirmChar = confirm("Do you want your password to contain special characters?");
+if (confirmChar){
+  passwordSet += special;
+}
+confirmLower = confirm("Do you want your password to contain lowercase letters?");
+if (confirmLower){
+  passwordSet += lower;
+}
   confirmUpper = confirm("Do you want your password to contain uppercase letters?")
+if (confirmUpper) {
+  passwordSet += upper;
 }
+
+// for loop to generate password of desired length
+if (charNum >= 8 && charNum <= 128){
+  for (var i = 0; i < charNum; i++){
+    newPassword += passwordSet.charAt(Math.floor(Math.random() * passwordSet.length));
+  };
+  localStorage.setItem("password", newPassword);
+  let newPasswordStr = localStorage.getItem("password");
+  passwordTextArea.textContent = newPasswordStr;
 }
+console.log(passwordSet)
+}
+
+
